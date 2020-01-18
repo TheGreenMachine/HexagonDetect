d97876a3c1e26329a9468013558a1a94c0ae685f
Ian McVann <ian.mcvann@gmail.com>
2020-01-18 11:06:50 -0600

    Tuned realcanny with real values

--- realcanny.py
+++ realcanny.py
@@ -6,7 +6,13 @@ from torch.autograd import Variable
 import numpy as np
 import math
 
+lowerbound = 0
 # create an array of points in the shape of a hexagon
+
+def sliderUpdate(val):
+    global lowerbound
+    lowerbound = val
+    print(lowerbound)
 def make_hex_shape():
     pts = []
     for ang in range(0, 355, 60):
@@ -17,7 +23,7 @@ def make_hex_shape():
     shape_np = np.array(pts, np.int32)
     shape_np = np.reshape(shape_np, (-1, 1, 2))
     return shape_np
-def procImage(img, shape):
+def procImage(img, shape, val):
     edges = cv2.Canny(img, 100, 300)
 
     cv2.imshow("edges", edges)
@@ -29,15 +35,15 @@ def procImage(img, shape):
     for cont in contours:
         rect = cv2.boundingRect(cont)
         # only process larger areas with at least 5 points in the contour
-        if len(cont) > 20 and rect[2] > 40 and rect[3] > 40:
+        if len(cont) > 80 and rect[2] > 55 and rect[3] > 55:
             match = cv2.matchShapes(cont, hex, cv2.CONTOURS_MATCH_I2, 0.0)
             if match < 0.08:
-                print(f"len(cont)={len(cont):3d}:  match={match:.4f}")
-
-            if match < 0.0125:
+                print("SKIP")
+            if match < .0165:
                 cx = rect[0] + (rect[2] * .5)
                 cy = rect[1] + (rect[3] * .5)
                 centerpoints = (cx, cy)
+                print(f"len(cont)={len(cont):3d}:  match={match:.4f}")
                 hexes.append(cont)
     img = cv2.drawContours(img, hexes, -1, (0, 255, 0), 4)
     ctr = np.array(centerpoints).reshape((-1, 1, 2)).astype(np.int32)
@@ -62,15 +68,17 @@ if __name__ == '__main__':
         frame_width = int(cap.get(3))
         frame_height = int(cap.get(4))
         out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width, frame_height))
-
+    cv2.namedWindow("edges")
+    cv2.createTrackbar("lower", "edges", 0, 100, sliderUpdate)
     while True:
         retr, img = cap.read()
         img = img[0:360, 0:1280]
-        img = procImage(img, hex)
+
+        img = procImage(img, hex, lowerbound)
         if args.save:
             out.write(img)
         cv2.imshow("contours", img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if cv2.waitKey(30) & 0xFF == ord('q'):
             break
     cap.release()
     out.release()
